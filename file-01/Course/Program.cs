2edb4e965a236c62f7dfbb983dd32bb7db67ff20
Douglas Borges <douglasborges19@gmail.com>
2023-05-15 21:20:47 -0300

    initial commit

---
+++ file-01/Course/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"d:\temp\file1.txt";
+            string targetPath = @"d:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
