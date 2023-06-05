9e6556cff6e6d03f651a9b7c37503095df6b549e
Douglas Borges <douglasborges19@gmail.com>
2023-06-05 19:39:21 -0300

    streamwriter

---
+++ file-04/Course/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"d:\temp\file1.txt";
+            string targetPath = @"d:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
