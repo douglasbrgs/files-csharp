72f54c38639457bd1a3d670e8cb1b1b6e7941cdc
Douglas Borges <douglasborges19@gmail.com>
2023-06-05 21:11:04 -0300

    exercise

---
+++ file-07/Course/Program.cs
@@ -0,0 +1,48 @@
+﻿using Course.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the file path: ");
+                string sourcePath = Console.ReadLine();
+
+                IEnumerable<string> lines = File.ReadAllLines(sourcePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product product = new Product(name, price, quantity);
+
+                        sw.WriteLine(product.Name + "," + product.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
